89611168f571279dfa1aa79fbcdb7dc8bf2ca8b7
Brian Kohan <bckohan@gmail.com>
2026-03-11 11:18:52 -0700

    fix app config name

--- {{cookiecutter.project_slug}}/src/{{cookiecutter.package_name}}/apps.py
+++ {{cookiecutter.project_slug}}/src/{{cookiecutter.package_name}}/apps.py
@@ -1,6 +1,6 @@
 from django.apps import AppConfig
 
 
-class AppConfig(AppConfig):
+class {{cookiecutter.project_slug.replace('-', '').replace('_', '').title()}}Config(AppConfig):
     name = "{{cookiecutter.package_name}}"
     verbose_name = "{{cookiecutter.project_slug.replace('-', ' ').replace('_', ' ').title()}}"
