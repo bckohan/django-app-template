20bcc142cf2c1eecffe5d0db92b24603cc0403e9
Brian Kohan <bckohan@gmail.com>
2026-03-11 10:27:50 -0700

    fix verbose name

--- {{cookiecutter.project_slug}}/src/{{cookiecutter.package_name}}/apps.py
+++ {{cookiecutter.project_slug}}/src/{{cookiecutter.package_name}}/apps.py
@@ -3,4 +3,4 @@ from django.apps import AppConfig
 
 class AppConfig(AppConfig):
     name = "{{cookiecutter.package_name}}"
-    verbose_name = "{{cookiecutter.project_slug}}"
+    verbose_name = "{{cookiecutter.project_slug.replace('-', ' ').replace('_', ' ').title()}}"
